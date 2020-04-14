8fa6f06fbaa6807ab303ed0435c418980bdb3a36
cleiton A silva <cleiton99788869@hotmail.com>
2020-04-13 21:35:50 -0300

    adicionando-carrosel

---
+++ inc/head.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes, maximum-scale=5.0, minimum-scale=1.0">
+    <title>Template site bootstrap</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        
+</head>
